3d0bca1d028efb9123109fc4c67091d3f9e5af07
marcin-bojar <marcin.bojar@interia.pl>
2020-12-20 23:10:36 +0100

    Installed framer-motion library, animating homepage heading with it

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import {
   searchInputSelector,
@@ -18,29 +19,50 @@ import './homepage.styles.scss';
 const AdsPreviewWithLoader = withLoader(AdsPreview);
 
 const HomePage = ({ searchInput, filtersApplied, isFetching }) => {
-  const hide = searchInput.length > 0 || filtersApplied;
+  const show = !searchInput.length > 0 && !filtersApplied;
 
   return (
-    <div className={`${hide ? 'hide-header' : ''} homepage`}>
-      <div className="hero-header">
-        <h1>
-          <span className="hero-header__color-letter">C</span>argo{' '}
-          <span className="hero-header__color-letter">R</span>unner
-        </h1>
-        <h2>Praca w logistyce</h2>
-      </div>
-      <div className="data-container">
-        <div className="data-container__ads">
-          {hide ? (
-            <SearchResults />
-          ) : (
-            <AdsPreviewWithLoader isLoading={isFetching} />
-          )}
-        </div>
-        <div className="data-container__search">
-          <Search />
-        </div>
-      </div>
+    <div className={`homepage`}>
+      <AnimatePresence initial={false}>
+        {/* {isFetching && (
+          <motion.div
+            key="loading"
+            exit={{ scaleY: 0 }}
+            transition={{ ease: 'easeIn' }}
+            className="black"
+          >
+            <motion.span>Loading</motion.span>
+          </motion.div>
+        )} */}
+        {show && (
+          <motion.div
+            key="header"
+            initial={{ height: 0 }}
+            animate={{ height: '45vh' }}
+            transition={{ duration: 0.5, ease: 0 }}
+            exit={{ height: 0 }}
+            className="hero-header"
+          >
+            <h1>
+              <span className="hero-header__color-letter">C</span>argo{' '}
+              <span className="hero-header__color-letter">R</span>unner
+            </h1>
+            <h2>Praca w logistyce</h2>
+          </motion.div>
+        )}
+        <motion.div key="data" className="data-container">
+          <div className="data-container__ads">
+            {!show ? (
+              <SearchResults />
+            ) : (
+              <AdsPreviewWithLoader isLoading={isFetching} />
+            )}
+          </div>
+          <div className="data-container__search">
+            <Search />
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
